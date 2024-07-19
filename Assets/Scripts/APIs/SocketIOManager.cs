06d14d581b120bca458d981e6ba258602031ed1c
EkanshBatra <>
2024-07-19 11:31:18 +0530

    socket change

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -34,7 +34,7 @@ public class SocketIOManager : MonoBehaviour
     [SerializeField]
     internal JSHandler _jsManager;
 
-    protected string SocketURI = "https://7p68wzhv-5000.inc1.devtunnels.ms/";
+    protected string SocketURI = "https://dev.casinoparadize.com";
 
     [SerializeField]
     private string TestToken;
